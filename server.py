c533823931c54ddf5e73bc029f63e4309475fadf
Juan Esteban Trillos <124514988+JuanexTm@users.noreply.github.com>
2024-05-14 12:23:20 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/’)
-    return render_template(‘teka-gas.html’) 	
+    return render_template(‘template/teka-gas.html’) 	
 
 if __name__ == "__main__":
     app.run (host="0.0.0.0", port=5000)
