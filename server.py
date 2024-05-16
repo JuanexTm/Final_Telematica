120130d7a5b2d93e3599742fbcd0ca20bbe5d357
Juan Esteban Trillos <124514988+JuanexTm@users.noreply.github.com>
2024-05-16 10:49:05 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/’)
-#def hello_world():
+def hello_world():
     #return "Hola, mundo!"
     return render_template(‘teka-gas.html’) 	
 
