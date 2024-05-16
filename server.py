117a43b290ed7a2b66cee90434f5dc70d0cf5c9e
Juan Esteban Trillos <124514988+JuanexTm@users.noreply.github.com>
2024-05-16 11:01:01 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/’)
 def hello_world():
-    return "Hola, mundo!"
-    #return render_template(‘teka-gas.html’) 	
+    #return "Hola, mundo!"
+    return render_template(‘teka-gas.html’) 	
 
 if __name__ == "__main__":
     app.run (host="0.0.0.0", port=5000)
